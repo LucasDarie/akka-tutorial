08134293a5d23b364ac5d24545441f3363301f3b
Lucas Darie <lucas.darie@etu.umontpellier.fr>
2023-06-06 10:09:33 +0200

    adding User class, representing data stored in the password.csv file

---
+++ ddm-exercise/src/main/java/de/hpi/ddm/utils/User.java
@@ -0,0 +1,19 @@
+package de.hpi.ddm.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private int id;
+    private String name;
+    // all character that can be present in the password (i.e. "ABCDEFGHIJK")
+    private String passwordChars;
+    private int passwordLength;
+    private String hashedPassword;
+    private List<String> hashedHints;
+}
